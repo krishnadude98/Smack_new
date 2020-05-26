f2f9dd8f9953d3b8ff402ede2cafb0498fda0c66
hari <krishnadude98@gmail.com>
2020-05-26 16:38:08 +0530

    lession5

--- app/src/main/java/com/hari/smack/CreateUserActivity.kt
+++ app/src/main/java/com/hari/smack/CreateUserActivity.kt
@@ -1,19 +1,46 @@
 package com.hari.smack
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import kotlin.random.Random
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar= "profileDefault"
+    var avatarColor="[0.5,0.5,0.5,1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
     fun generateUserAvatar(view: View){
+        val random = Random
+        val color = random.nextInt(2)
+        val avatar= random.nextInt(28)
+        if(color==0){
+            userAvatar= "light$avatar"
+        }
+        else{
+            userAvatar= "dark$avatar"
+        }
+        val resourceId= resources.getIdentifier(userAvatar,"drawable",packageName)
+        createAvatarImageView.setImageResource(resourceId)
 
     }
     fun generateColorClicked(view: View){
+        val random = Random
+        val r= random.nextInt(255)
+        val g= random.nextInt(255)
+        val b = random.nextInt(255)
+        createAvatarImageView.setBackgroundColor(Color.rgb(r,g,b))
+        val savedR= r.toDouble()/255
+        val savedg= g.toDouble()/255
+        val savedb=b.toDouble()/255
+        avatarColor= "[$savedR,$savedg,$savedb,1]"
+        
 
     }
     fun createUserBtnClicked(view: View){
